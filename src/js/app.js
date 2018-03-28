9411d3e217db183cf345690a62a87dda8b69dac9
Antonio Voce <antonio.voce@guardian.co.uk>
2018-03-28 12:57:56 +0100

    Add circle labels logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
 import makeMonthSvgs from './makeMonth';
 import * as d3 from "d3";
 import { swoopyDrag } from 'd3-swoopy-drag';
+import loadJson from '../components/load-json';
 
 // helper funcs
-const calcXDatePosition = (date, cellSize) => date.getDay() * cellSize;
-const calcYDatePosition = (date, cellSize) => d3.timeWeek.count(d3.timeMonth(date), date) * cellSize;
 const isSameDay = (dateToCheck, actualDate) => { return dateToCheck.getDate() === actualDate.getDate() && dateToCheck.getMonth() === actualDate.getMonth() && dateToCheck.getFullYear() === actualDate.getFullYear() };
 
 // config variables
@@ -12,12 +11,10 @@ const cellSize = Math.floor(document.querySelector(".interactive-atom").clientWi
 const cellSizeMargin = cellSize - 10;
 const container = document.querySelector('.months-container');
 const december = document.querySelector('.december');
-const november = document.querySelector('.november');
 
 const counterSticky = document.querySelector('.counter-number');
 const counterMonth = document.querySelector('.counter-month')
 const domElements = document.querySelectorAll('.cal-month'); // months need to be named correctly in the css classes, all lowercase
-const genders = ['women', 'men'];
 let size = 0;
 let lastScroll = null;
 
@@ -25,127 +22,154 @@ var formatMonth = d3.timeFormat("%e %B");
 
 makeMonthSvgs(domElements, cellSize);
 
-// parse csv and do stuff
-d3.csv(process.env.PATH + "/assets/latest.csv", function(error, csv) {
-    if (error) throw error;
+//load annotations, parse csv and do stuff
+loadJson('https://interactive.guim.co.uk/docsdata-test/1BxXGXMice-3-fCx61MLLDzx18bsE-n85w1SbaWHjiWE.json').then(res => {
+    let circleAnnotations = res.annotations;
+    
+    d3.csv(process.env.PATH + "/assets/latest.csv", function(error, csv) {
+        if (error) throw error;
 
-    var totalCompaniesReporting = 0;
-    //get number of weekdays in the year
-    var totalWeekDays = getTotalWeekDays();
-
-    var dates = [];
-
-    // NB POSSIBLY REVIEW: HAD TO CHANGE THIS FROM < TO <= TO PREVENT IT FROM BREAKING WITH THE NEW DATASET
-    for (var i = 0; i <= totalWeekDays; i++) {
-        dates.push({
-            womenPaidLess: 0,
-            menPaidLess: 0,
-            isWeekend: false
-        })
-    }
-
-    //group company totals into days on a calendar
-    csv.forEach(d => {
-        let lower = Number(d.DiffMedianHourlyPercent);
-        let val = 1;
-        totalCompaniesReporting += val;
-        let day = totalWeekDays - Math.floor(Math.abs(lower) / 100 * totalWeekDays);
-
-        if (lower === 0) {
-            //skip
-        } else if (lower > 0) {
-            //women paid less
-            dates[day].womenPaidLess += val;
-        } else if (lower < 0 && lower > -2000) {
-            //men paid less
-            dates[day].menPaidLess += val;
-        }
-    });
-
-    //add back the weekend days
-    dates = addsWeekends(dates);
-
-    //creates total aggregate
-    var totalWomenCounter = 0;
-    var totalMenCounter = 0;
-    dates.forEach(function(d) {
-        totalWomenCounter += d.womenPaidLess;
-        totalMenCounter += d.menPaidLess;
-        d.womenTotalPaidLess = totalWomenCounter;
-        d.menTotalPaidLess = totalMenCounter;
-    })
-    var maxTotal = (totalWomenCounter > totalMenCounter) ? totalWomenCounter : totalMenCounter;
-    var maxPct = maxTotal / totalCompaniesReporting;
-
-    //creates percents of totals
-    dates.forEach(function(d) {
-        d.womenPctPaidLess = d.womenTotalPaidLess / totalCompaniesReporting;
-        d.menPctPaidLess = d.menTotalPaidLess / totalCompaniesReporting;
-    });
-
-    // addData(dates, totalWomenCounter);
-    addData(dates, domElements);
-
-    function checkScroll() {
-        if (lastScroll !== window.pageYOffset) {
-            onScroll(domElements, cellSize);
-            size = d3.selectAll(".has-data").size();
-            lastScroll = window.pageYOffset;
-        }
-        window.requestAnimationFrame(() => {
-            checkScroll();
+        const csvWithHighlights = csv.map(d => {
+            const index = circleAnnotations.map(f => Number(f.gap)).indexOf(Number(d.DiffMedianHourlyPercent));
+            if (index > -1) {
+                d.highlighted = true;
+                d.companyName = circleAnnotations[index].company;
+                circleAnnotations.splice(index, 1);
+            }
+            return d;
         });
-    }
 
-    checkScroll();
 
-    /* Swoopy arrow stuff */
 
-    const annotations = [{
-        "dateX": 40,
-        "dateY": -224,
-        "path": "M-21,274C-21,315,-10,350,50,357",
-        "text": "These are the companies that stop paying women one day earlier than men",
-        "textOffset": [-34,
-            261
-        ]
-    }]
+        var totalCompaniesReporting = 0;
+        //get number of weekdays in the year
+        var totalWeekDays = getTotalWeekDays();
 
-    const decemberSvg = d3.select(december)
-        .select("svg")
+        var dates = [];
 
-    const swoopy = swoopyDrag()
-        // .draggable(true)
-        .x(d => d.dateX)
-        .y(d => d.dateY)
-        .on('drag', () => window.annotations = annotations)
-        .annotations(annotations)
+        // NB POSSIBLY REVIEW: HAD TO CHANGE THIS FROM < TO <= TO PREVENT IT FROM BREAKING WITH THE NEW DATASET
+        for (var i = 0; i <= totalWeekDays; i++) {
+            dates.push({
+                womenPaidLess: 0,
+                menPaidLess: 0,
+                isWeekend: false,
+                highlightCompanyName: null,
+                highlighted: false
+                // payGaps: []
+            })
+        }
 
-    const swoopySel = decemberSvg.select('.swoopy-arrow-group').call(swoopy);
+        //group company totals into days on a calendar
+        csvWithHighlights.forEach(d => {
+            let lower = Number(d.DiffMedianHourlyPercent);
+            let val = 1;
+            totalCompaniesReporting += val;
+            let day = totalWeekDays - Math.floor(Math.abs(lower) / 100 * totalWeekDays);
 
-    // arrow path
-    swoopySel.selectAll('path')
-        // .attr('class', d => `swoopy-path-${d.dateY}`)
-        .attr('fill', 'none')
-        .attr('stroke', '#000')
-        .attr('stroke-opacity', 0)
-        .attr('marker-end', 'url(#arrow)')
+            if (lower === 0) {
+                //skip
+            } else if (lower > 0) {
+                //women paid less
+                // dates[day].payGaps.push(lower)
+                dates[day].womenPaidLess += val;
+                dates[day].highlightCompanyName = dates[day].highlightCompanyName || d.companyName;
+                dates[day].highlighted = dates[day].highlighted || d.highlighted;
+            } else if (lower < 0 && lower > -2000) {
+                //men paid less
+                dates[day].menPaidLess += val;
+            }
+        });
+        
+        //add back the weekend days
+        dates = addsWeekends(dates);
+        
 
-    // arrow tip
-    decemberSvg
-        .append('marker')
-        .attr('id', 'arrow')
-        .attr('fill-opacity', 0)
-        .attr('viewBox', '-10 -10 20 20')
-        .attr('markerWidth', 10)
-        .attr('markerHeight', 20)
-        .attr('orient', 'auto')
-        .append('path')
-        .attr('d', 'M-6.75,-6.75 L 0,0 L -6.75,6.75')
+        
 
-    swoopySel.selectAll('text')
-        .attr('fill-opacity', 0)
-});
+        //creates total aggregate
+        var totalWomenCounter = 0;
+        var totalMenCounter = 0;
+        dates.forEach(function(d) {
+            totalWomenCounter += d.womenPaidLess;
+            totalMenCounter += d.menPaidLess;
+            d.womenTotalPaidLess = totalWomenCounter;
+            d.menTotalPaidLess = totalMenCounter;
+        })
+        var maxTotal = (totalWomenCounter > totalMenCounter) ? totalWomenCounter : totalMenCounter;
+        var maxPct = maxTotal / totalCompaniesReporting;
+
+        //creates percents of totals
+        dates.forEach(function(d) {
+            d.womenPctPaidLess = d.womenTotalPaidLess / totalCompaniesReporting;
+            d.menPctPaidLess = d.menTotalPaidLess / totalCompaniesReporting;
+        });
+
+
+        
+        // addData(dates, totalWomenCounter);
+        addData(dates, domElements);
+
+        function checkScroll() {
+            if (lastScroll !== window.pageYOffset) {
+                onScroll(domElements, cellSize);
+                size = d3.selectAll(".has-data").size();
+                lastScroll = window.pageYOffset;
+            }
+            window.requestAnimationFrame(() => {
+                checkScroll();
+            });
+        }
+
+        checkScroll();
+
+        /* Swoopy arrow stuff */
+
+        const annotations = [{
+            "dateX": 40,
+            "dateY": -224,
+            "path": "M-21,274C-21,315,-10,350,50,357",
+            "text": "These are the companies that stop paying women one day earlier than men",
+            "textOffset": [-34,
+                261
+            ]
+        }]
+
+        const decemberSvg = d3.select(december)
+            .select("svg")
+
+        const swoopy = swoopyDrag()
+            // .draggable(true)
+            .x(d => d.dateX)
+            .y(d => d.dateY)
+            .on('drag', () => window.annotations = annotations)
+            .annotations(annotations)
+
+        const swoopySel = decemberSvg.select('.swoopy-arrow-group').call(swoopy);
+
+        // arrow path
+        swoopySel.selectAll('path')
+            // .attr('class', d => `swoopy-path-${d.dateY}`)
+            .attr('fill', 'none')
+            .attr('stroke', '#000')
+            .attr('stroke-opacity', 0)
+            .attr('marker-end', 'url(#arrow)')
+
+        // arrow tip
+        decemberSvg
+            .append('marker')
+            .attr('id', 'arrow')
+            .attr('fill-opacity', 0)
+            .attr('viewBox', '-10 -10 20 20')
+            .attr('markerWidth', 10)
+            .attr('markerHeight', 20)
+            .attr('orient', 'auto')
+            .append('path')
+            .attr('d', 'M-6.75,-6.75 L 0,0 L -6.75,6.75')
+
+        swoopySel.selectAll('text')
+            .attr('fill-opacity', 0)
+    })
+})
 
 
 // should this take an array of month elements?
@@ -165,20 +189,21 @@ const addData = (dates, domElements) => {
             const filteredDates = dates.slice(firstDayIndex, lastDayIndex + 1);
             d3.select(domElement).selectAll(".day-group").data(filteredDates);
 
-            d3.select(domElement).selectAll(".day-group")
+            const daysInMonth = d3.select(domElement).selectAll(".day-group")
+
+            const parent = daysInMonth
                 .selectAll("circle")
                 .data(d => { return new Array(d['womenPaidLess']).fill(d) })
-                .enter()
+                .enter();
+            
+            const circles = parent
                 .append("circle")
                 .attr('class', 'dayData')
                 .attr('fill', '#ff7e00')
                 .attr('opacity', 0)
                 .attr('r', 3)
-                // .style("transform", d => {
-                //     const x = (Math.random() - 0.5) * 10;
-                //     const y = (Math.random() - 0.5) * 10;
-                //     return `translate(${x}px,${y}px)`;
-                // })
+
+            circles
                 .each(function(d, i, a) {
                     const node = d3.select(this);
 
@@ -197,7 +222,34 @@ const addData = (dates, domElements) => {
                         })
                         .attr('cy', (d, i, a) => s[1] + 5)
                 });
+            
+            const firstHighlight = parent.filter(d => d.highlighted).filter((d, i) => i === 0)
 
+            firstHighlight.append("text")
+                .classed('circle-label', true)
+                .attr("x", cellSize / 2)
+                .attr('y', cellSize / 2)
+                .attr("text-anchor", "middle")
+                .attr('dy', 18)
+                .style('stroke', 'white')
+                .style('stroke-width', '3px')
+                .style('opacity', 0)
+                .text(d => d.highlightCompanyName)
+
+            
+            firstHighlight.append('text')
+                .classed('circle-label-outline', true)
+                .attr("x", cellSize / 2)
+                .attr('y', cellSize / 2)
+                .attr('dy', 18)
+                .attr("text-anchor", "middle")
+                .style('opacity', 0)
+                .text(d => d.highlightCompanyName)
+
+
+            daysInMonth.select('.dayData')
+                .attr('r', d => d.highlighted && 6)
+                .attr('stroke', d => d.highlighted && 'black')
 
             d3.select(domElement).selectAll(".day-group")
                 .classed('weekend', function(d) {
@@ -360,8 +412,6 @@ const calcCirclePos = (d, i, a, xOrY) => {
 
     // const unit = cellSize / grid;
 
-    // console.log(x, y, unit);
-
     // if (xOrY === "x") {
     //     return randomPos(x * unit);
     // } else {
@@ -393,7 +443,6 @@ const onScroll = (domElements, cellSize) => {
             });
 
         if (!element.weekEls) {
-            console.log("f")
             element.weekEls = element.querySelectorAll(".week-group");
         }
 
@@ -408,88 +457,46 @@ const onScroll = (domElements, cellSize) => {
                 });
             }
 
+            d3.select(group).selectAll('.circle-label')
+                .style('opacity', groupRect.top < 482 ? 1 : 0)
 
-            /* Swoopy arrows stuff */
-            const elemRect = element.getBoundingClientRect();
-            const arrows = d3.select(element).select('.swoopy-arrow-group');
-
-            arrows.selectAll('path')
-                .transition()
-                .delay(0)
-                .ease(d3.easeExpOut)
-                .duration(2000)
-                .attr('stroke-opacity', elemRect.top < 396 ? 1 : 0)
-                .attr('marker-end', 'url(#arrow)')
-
-            arrows.selectAll('text')
-                .transition()
-                .delay(0)
-                .ease(d3.easeExpOut)
-                .duration(2000)
-                .attr('fill-opacity', elemRect.top < 396 ? 1 : 0)
-
-            d3.select(element).selectAll('marker')
-                .transition()
-                .delay(0)
-                .ease(d3.easeExpOut)
-                .duration(2000)
-                .attr('fill-opacity', elemRect.top < 396 ? 1 : 0)
-
-            // if (elemRect.top > 500) {
-            //     d3.select(group).selectAll(".dayData")
-            //         .transition()
-            //         .delay(i => {
-            //             return Math.random() * 500;
-            //         })
-            //         .ease(d3.easeExpOut)
-            //         .duration(250)
-            //         .style("opacity", "0")
-            //         .attr('r', 2.5)
-
-            // }
-
-            // d3.select(group).selectAll("text")
-            //     .transition()
-            //     .delay((d, i) => i * 100)
-            //     .ease(d3.easeExpOut)
-            //     .duration(2000)
-            //     .style("opacity", d => elemRect.top < 500 ? "1" : "0");
+            d3.select(group).select('.circle-label-outline')
+                .style('opacity', groupRect.top < 482 ? 1 : 0)
         });
 
         /* Swoopy arrows stuff */
-        // arrows.selectAll('path')
-        //   .transition()
-        //   .delay(0)
-        //   .ease(d3.easeExpOut)
-        //   .duration(4000)
-        //   .attr('stroke-opacity', d => elemRect.top + 508 < windowCenter ? 1 : 0)
-        //   .attr('marker-end', 'url(#arrow)')
+        const elemRect = element.getBoundingClientRect();
+        const arrows = d3.select(element).select('.swoopy-arrow-group');
 
-        // arrows.selectAll('text')
-        //   .transition()
-        //   .delay(0)
-        //   .ease(d3.easeExpOut)
-        //   .duration(4000)
-        //   .attr('fill-opacity', 1)
+        arrows.selectAll('path')
+            .transition()
+            .delay(0)
+            .ease(d3.easeExpOut)
+            .duration(2000)
+            .attr('stroke-opacity', elemRect.top < 396 ? 1 : 0)
+            .attr('marker-end', 'url(#arrow)')
 
-        // d3Container.selectAll('marker')
-        //   .transition()
-        //   .delay(0)
-        //   .ease(d3.easeExpOut)
-        //   .duration(4000)
-        //   .attr('fill-opacity', 1)
+        arrows.selectAll('text')
+            .transition()
+            .delay(0)
+            .ease(d3.easeExpOut)
+            .duration(2000)
+            .attr('fill-opacity', elemRect.top < 396 ? 1 : 0)
 
-        /* Highilighting parts of calendar (not working) */
-        // d3.select('.gv-w').select("rect[id='2018-01-20']")
-        //   .attr('fill', d => console.log(this))
-        //   .attr('fill-opacity', 1)
+        d3.select(element).selectAll('marker')
+            .transition()
+            .delay(0)
+            .ease(d3.easeExpOut)
+            .duration(2000)
+            .attr('fill-opacity', elemRect.top < 396 ? 1 : 0)
+
     });
 
     counterMonth.innerHTML = formatMonth(latestWeek);
-
 }
 
 const transitionCircles = (group, groupRect) => {
+
     d3.select(group).selectAll(".day-group").selectAll(".dayData")
         .classed('has-data', d => groupRect.top < 500 && d['womenPaidLess'] > 0)
         .transition()
@@ -502,7 +509,9 @@ const transitionCircles = (group, groupRect) => {
             return "none";
         })
         .style("opacity", groupRect.top < 500 ? "1" : 0)
-        .attr('r', 2.5)
+        .attr('r', 3)
+
+    
 }
 
 const getTotalWeekDays = () => {
